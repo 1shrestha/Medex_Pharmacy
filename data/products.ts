ae619e078db99f583d90728a5be88f6f0359a483
Shrestha pandey <pandeyshree141@gmail.com>
2026-08-08 12:36:56 +0530

    Add files via upload

---
+++ data/products.ts
@@ -0,0 +1,25 @@
+export const products = [
+  {
+    id: 1,
+    name: "Dolo 650",
+    price: 35,
+    image: "/dolo.jpg",
+    category: "Fever"
+  },
+
+  {
+    id: 2,
+    name: "Paracetamol",
+    price: 25,
+    image: "/paracetamol.jpg",
+    category: "Pain Relief"
+  },
+
+  {
+    id: 3,
+    name: "Vitamin C",
+    price: 120,
+    image: "/vitaminc.jpg",
+    category: "Supplements"
+  }
+];
